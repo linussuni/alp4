971de89e5efb27a644587dbf8b7213dd2673f1f6
Linus Ziegert <linus.ziegert@holoplot.com>
2019-06-11 16:37:53 +0200

    u7: math still broken, add write to csv

--- u7/u7_1.c
+++ u7/u7_1.c
@@ -4,7 +4,10 @@
 #include <time.h>
 
 #define NUM_OBJ 5
-#define GRAVITY_CONSTANT (6.674 * pow(10, -5))
+#define GRAVITY_CONSTANT 1.0
+//#define GRAVITY_CONSTANT (6.674 * pow(10, -11))
+#define ZERO_VECTOR vector zero_vector = {0.0, 0.0};
+#define DT 0.00001
 
 typedef struct
 {
@@ -44,14 +47,27 @@ double len_vector(vector a)
 }
 
 // printing functions
-void print_obj_pos_speed(obj o[])
+void print_obj_pos_speed(obj o[], int cycle)
 {
-    printf("%-8s%-24s%-24s%-24s%-24s\n", "ID", "pos x", "pos y", "speed", "mass");
-    for (int i = 0; i < NUM_OBJ; i++)
+    if (cycle < 0)
     {
-        double speed = len_vector(o[i].velocity);
-        printf("%-8d%-24f%-24f%-24f%-24f\n",
-               o[i].id, o[i].position.x, o[i].position.y, speed, o[i].mass);
+        printf("%-8s%-24s%-24s%-24s%-24s\n", "ID", "pos x", "pos y", "speed", "mass");
+        for (int i = 0; i < NUM_OBJ; i++)
+        {
+            double speed = len_vector(o[i].velocity);
+            printf("%-8d%-24f%-24f%-24f%-24f\n",
+                   o[i].id, o[i].position.x, o[i].position.y, speed, o[i].mass);
+        }
+    }
+    else
+    {
+        printf("cycle: %-5d%-8s%-24s%-24s%-24s%-24s\n", cycle, "ID", "pos x", "pos y", "speed", "mass");
+        for (int i = 0; i < NUM_OBJ; i++)
+        {
+            double speed = len_vector(o[i].velocity);
+            printf("            %-8d%-24f%-24f%-24f%-24f\n",
+                   o[i].id, o[i].position.x, o[i].position.y, speed, o[i].mass);
+        }
     }
 }
 
@@ -63,8 +79,38 @@ void print_obj_all(obj o[])
         printf("%-8d%-24f%-24f%-24f%-24f%-24f%-24f%-24f\n",
                o[i].id, o[i].position.x, o[i].position.y,
                o[i].velocity.x, o[i].velocity.y,
-               o[i].velocity.x, o[i].velocity.y, o[i].mass);
+               o[i].acceleration.x, o[i].acceleration.y,
+               o[i].mass);
     }
+}
+
+// object handlers
+obj *create_obj(int i)
+{
+    obj *o = malloc(sizeof(obj));
+
+    o->id = i;
+    o->position.x = rand() % 100;
+    o->position.y = rand() % 100;
+    o->velocity.x = 0.0;
+    o->velocity.y = 0.0;
+    o->acceleration.x = 0.0;
+    o->acceleration.y = 0.0;
+    o->mass = rand() % 1000000;
+
+    return o;
+}
+
+obj destroy_obj(obj o)
+{
+    ZERO_VECTOR
+    o.id = -1;
+    o.mass = 0;
+    o.acceleration = zero_vector;
+    o.velocity = zero_vector;
+    o.position = zero_vector;
+
+    return o;
 }
 
 // acceleration, velocity, position functions
@@ -73,78 +119,105 @@ void update_position(obj o[])
 {
     for (int i = 0; i < NUM_OBJ; i++)
     {
-        // drift of o[i]
-        vector drift = add_vectors(o[i].velocity, scalar_vector(o[i].acceleration, 0.5));
-        o[i].position = add_vectors(o[i].position, drift);
+        if (o[i].id != -1)
+        {
+            // drift of o[i]
+            vector drift = add_vectors(scalar_vector(o[i].velocity, DT), scalar_vector(o[i].acceleration, 0.5 * DT * DT));
+            o[i].position = add_vectors(o[i].position, drift);
+        }
     }
 }
 
 void update_velocity(obj o[])
 {
     for (int i = 0; i < NUM_OBJ; i++)
-    {
-        o[i].velocity = add_vectors(o[i].velocity, o[i].acceleration);
-    }
+        if (o[i].id != -1)
+        {
+            {
+                o[i].velocity = add_vectors(o[i].velocity, o[i].acceleration);
+            }
+        }
 }
 
 void update_acceleration(obj o[])
 {
-    vector zero_vector = {0.0, 0.0};
+    ZERO_VECTOR
     for (int i = 0; i < NUM_OBJ; i++)
     {
-        o[i].acceleration = zero_vector;
-        for (int j = 0; j < NUM_OBJ; j++)
+        if (o[i].id != -1)
         {
-            if (i != j)
+            o[i].acceleration = zero_vector;
+            for (int j = 0; j < NUM_OBJ; j++)
             {
-                // calculate attractive force F = G * (mass / r^2)
-                double distance = len_vector(sub_vectors(o[i].position, o[j].position));
-                double attractive_force = GRAVITY_CONSTANT * o[j].mass / pow(distance, 2);
-
-                // calculate effect on acceleration of obj j on i
-                vector acceleration_impact = scalar_vector(sub_vectors(o[j].position, o[i].position), attractive_force);
-
-                // add acceleration_impact
-                o[i].acceleration = add_vectors(o[i].acceleration, acceleration_impact);
-            }
-        }
-    }
-}
-void update_collision(obj o[])
-{
-    for (int i = 0; i < NUM_OBJ; i++)
-    {
-        for (int j = 0; j < NUM_OBJ; j++)
-        {
-            if (i != j)
-            {
-                if (round(o[i].position.x) == round(o[j].position.x) && round(o[i].position.y) == round(o[j].position.y))
+                if (i != j && o[j].id != -1)
                 {
-                    printf("x: %f y:%f", round(o[i].position.x), round(o[j].position.x));
-                    o[i].id = -1;
-                    o[j].mass += o[i].mass;
-                    o[j].velocity = add_vectors(o[j].velocity, o[i].velocity);
-                    o[j].acceleration = add_vectors(o[j].acceleration, o[i].acceleration);
+                    // calculate attractive force F = G * (mass / r^2)
+                    double distance = len_vector(sub_vectors(o[i].position, o[j].position));
+                    double attractive_force = GRAVITY_CONSTANT * o[j].mass / pow(distance, 2);
+
+                    // calculate effect on acceleration of obj j on i
+                    vector acceleration_impact = scalar_vector(sub_vectors(o[j].position, o[i].position), attractive_force);
+
+                    // add acceleration_impact
+                    o[i].acceleration = add_vectors(o[i].acceleration, acceleration_impact);
                 }
             }
         }
     }
 }
-// init objects
-obj *create_obj(int i)
+
+void handle_collision(obj o[])
 {
-    obj *o = malloc(sizeof(obj));
+    for (int i = 0; i < NUM_OBJ; i++)
+    {
+        if (o[i].id != -1)
+        {
+            for (int j = 0; j < NUM_OBJ; j++)
+            {
+                if (i != j && o[j].id != -1)
+                {
+                    if (round(o[i].position.x) == round(o[j].position.x) && round(o[i].position.y) == round(o[j].position.y))
+                    {
+                        o[j].mass += o[i].mass;
+                        o[j].velocity = add_vectors(o[j].velocity, o[i].velocity);
+                        o[j].acceleration = add_vectors(o[j].acceleration, o[i].acceleration);
+                        o[i] = destroy_obj(o[i]);
+                    }
+                }
+            }
+        }
+    }
+}
 
-    o->id = i;
-    o->position.x = rand() % 100;
-    o->position.y = rand() % 100;
-    o->velocity.x = 0;
-    o->velocity.y = 0;
-    o->acceleration.x = 0;
-    o->acceleration.y = 0;
-    o->mass = rand() % 1000000;
+void ignore_collision(obj o[])
+{
 
-    return o;
+}
+// plotting
+int init_csv(char const *filename)
+{
+    FILE *f = fopen(filename, "w");
+    if (f == NULL)
+    {
+        return -1;
+    }
+    fclose(f);
+    return 0;
+}
+
+int wirte_to_csv(obj o[], char const *filename)
+{
+    FILE *f = fopen(filename, "a");
+    if (f == NULL)
+    {
+        return -1;
+    }
+    for (int i = 0; i < NUM_OBJ; i++)
+    {
+        fprintf(f, "%d,%f,%f\n", o[i].id, o[i].position.x, o[i].position.y);
+    }
+    fclose(f);
+    return 0;
 }
 
 int main()
@@ -166,17 +239,21 @@ int main()
         obj *o = create_obj(i);
         obj_arr[i] = *o;
     }
-    obj_arr[0].position.x = 0.0;
-    obj_arr[0].position.y = 0.0;
-    obj_arr[0].mass = 100000000000;
+    obj_arr[NUM_OBJ - 1].position.x = 0.0;
+    obj_arr[NUM_OBJ - 1].position.y = 0.0;
+    obj_arr[NUM_OBJ - 1].mass = 1000000000;
 
+    // init csv file
+    init_csv("test.csv");
     print_obj_all(obj_arr);
-    for (int cycles = 0; cycles < 10; cycles++)
+    for (int cycle = 0; cycle < 200; cycle++)
     {
         update_acceleration(obj_arr);
-        update_velocity(obj_arr);
         update_position(obj_arr);
-        update_collision(obj_arr);
+        update_velocity(obj_arr);
+        ignore_collision(obj_arr);
+        wirte_to_csv(obj_arr, "test.csv");
+        //print_obj_pos_speed(obj_arr, cycle);
     }
     print_obj_all(obj_arr);
 
