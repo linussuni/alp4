6a4b52d3359714685b9ecf74b493c2b915e149a4
scared-fish <44439003+scared-fish@users.noreply.github.com>
2019-07-03 23:50:44 +0200

    Update JobMath.java

--- u10/alpv/mwp/JobMath.java
+++ u10/alpv/mwp/JobMath.java
@@ -1,1 +1,62 @@
 package alpv.mwp;
+
+import java.rmi.RemoteException;
+
+import alpv.mwp.Job;
+import alpv.mwp.Pool;
+import alpv.mwp.Task;
+import java.util;
+
+public class SquareJob implements Job<Integer, Integer, Integer> {
+
+	private static final long serialVersionUID = 267504255130640656L;
+	private Integer[] _numbers;
+	protected SquareTask _task;
+	protected Integer _argument;
+	protected SquareRemoteFuture _remoteFuture;
+	public SquareJob(Integer[] numbers) throws RemoteException {
+		_task = new SquareTask();
+		_numbers = numbers;
+	}
+
+	@Override
+	public void split(Pool<Integer> argPool, int workerCount) {
+		for (Integer number : _numbers) {
+			try {
+				argPool.put(number);
+			} catch (RemoteException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public SquareRemoteFuture getFuture() {
+		if(_remoteFuture == null) {
+			try {
+				_remoteFuture = new SquareRemoteFuture();
+			} catch (RemoteException e) {
+				e.printStackTrace();
+			}
+		}
+		return _remoteFuture;
+	}
+
+	public Task<Integer, Integer> getTask() {
+		return _task;
+	}
+
+	@Override
+	public void merge(Pool<Integer> resPool) {
+		Integer result = 0 ;
+		try {
+			Integer i;
+			while ((i = resPool.get()) != null) {
+				result += i
+			}
+			getFuture().set(result);
+		} catch (RemoteException e) {
+			// ignore
+		}
+		
+	}
+}
