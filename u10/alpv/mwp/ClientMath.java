ebf127eda9589559f3e3a52fe835a079c54e56d7
scared-fish <44439003+scared-fish@users.noreply.github.com>
2019-07-03 23:49:45 +0200

    Update ClientMath.java

--- u10/alpv/mwp/ClientMath.java
+++ u10/alpv/mwp/ClientMath.java
@@ -1,1 +1,24 @@
 package alpv.mwp;
+
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+
+import alpv.mwp.Client;
+import alpv.mwp.Job;
+import alpv.mwp.RemoteFuture;
+
+public class SquareClient extends Client {
+
+	public SquareClient(String host, int port) throws RemoteException,
+			NotBoundException {
+		super(host, port);
+	}
+
+	public void execute() throws RemoteException {
+		Integer[] numbers = { 2, 3, 4 };
+		Job<Integer, Integer, Integer> job = new SquareJob(numbers);
+
+		RemoteFuture<Integer> remoteFuture = _server.doJob(job);
+		System.out.println(remoteFuture.get());
+	}
+}
